c160d76f7be151780f4d2d5b978091c68708a12f
henrysasson <119268685+henrysasson@users.noreply.github.com>
2024-07-18 15:36:18 -0300

    Update trading_tool.py

--- trading_tool.py
+++ trading_tool.py
@@ -12,7 +12,7 @@ import streamlit as st
 st.set_page_config(page_title='Trading Tool', layout='wide')
 
 st.title('Trading Tool')
-    st.markdown('##')
+st.markdown('##')
 
 
 def quantile_of_points_in_data_series (data_series):
