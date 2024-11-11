0b7069370771b01bd1e2a4d28aad074473cb7447
henrysasson <119268685+henrysasson@users.noreply.github.com>
2024-11-11 15:59:25 -0300

    Update pre_trade.py

--- pre_trade.py
+++ pre_trade.py
@@ -1,17 +1,10 @@
 import pandas as pd
 import numpy as np
-import datetime
-from datetime import datetime
-import time
 import yfinance as yf
 import streamlit as st
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.express as px
-import warnings
-
-# Suppress FutureWarnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
 
 st.set_page_config(page_title='HS Pre Trade', layout='wide')
 
